0e87fbb68e1efb569de0679e9ccb13f13bb59ede
RobertttBS <k0983357798@gmail.com>
2023-12-12 05:15:07 +0800

    Add a script that configure tc etf and work properly.

---
+++ script/ns_ran_upf_2ue_etf_setup.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+source ./config
+EXEC_NS2="sudo ip netns exec ${NS2}"
+
+# Setup network namespace
+sudo ip netns add ${NS2}
+
+# Setup RAN part
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link set veth0 up
+sudo ip addr add ${UE_IP} dev lo
+sudo ip addr add ${UE2_IP} dev lo
+sudo ip addr add ${RAN_IP}/24 dev veth0
+
+sudo ip link set veth1 netns ${NS2}
+
+# Setup UPF part
+${EXEC_NS2} ip link set lo up
+${EXEC_NS2} ip link set veth1 up
+${EXEC_NS2} ip addr add ${DN_IP} dev lo
+${EXEC_NS2} ip addr add ${UPF_IP}/24 dev veth1
+
+if [ ${DUMP_NS} ]
+then
+    ${EXEC_NS2} tcpdump -i any -w ${NS2}.pcap &
+    TCPDUMP_PID=$(sudo ip netns pids ${NS2})
+fi
+
+cd ${LIBGTP5GNL_TOOLS_PATH}
+
+echo "############### RAN Part ###############"
+sudo ./gtp5g-link add gtp5gtest --ran &
+sleep 0.1
+sudo ./gtp5g-tunnel add qer gtp5gtest 123 --qfi 9
+sudo ./gtp5g-tunnel add qer gtp5gtest 321 --qfi 9
+sudo ./gtp5g-tunnel add qer gtp5gtest 11 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+sudo ./gtp5g-tunnel add qer gtp5gtest 12 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+# the parameter after <ifname> is oid (object identifier)
+sudo ./gtp5g-tunnel add far gtp5gtest 1 --action 2
+# --hdr-creation <description> <o-teid> <peer-ipv4> <peer-port>
+sudo ./gtp5g-tunnel add far gtp5gtest 2 --action 2 --hdr-creation 0 78 ${UPF_IP} 2152
+sudo ./gtp5g-tunnel add pdr gtp5gtest 1 --pcd 1 --hdr-rm 0 --ue-ipv4 ${UE_IP} --f-teid 87 ${RAN_IP} --far-id 1 --qer-id 11 # --pcd <precedence>
+sudo ./gtp5g-tunnel add pdr gtp5gtest 2 --pcd 2 --ue-ipv4 ${UE_IP} --far-id 2 --qer-id 12
+
+sudo ./gtp5g-tunnel add qer gtp5gtest 124 --qfi 9
+sudo ./gtp5g-tunnel add qer gtp5gtest 322 --qfi 9
+sudo ./gtp5g-tunnel add qer gtp5gtest 13 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+sudo ./gtp5g-tunnel add qer gtp5gtest 14 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+sudo ./gtp5g-tunnel add far gtp5gtest 3 --action 2
+sudo ./gtp5g-tunnel add far gtp5gtest 4 --action 2 --hdr-creation 0 79 ${UPF_IP} 2152
+sudo ./gtp5g-tunnel add pdr gtp5gtest 3 --pcd 1 --hdr-rm 0 --ue-ipv4 ${UE2_IP} --f-teid 88 ${RAN_IP} --far-id 3 --qer-id 13
+sudo ./gtp5g-tunnel add pdr gtp5gtest 4 --pcd 2 --ue-ipv4 ${UE2_IP} --far-id 4 --qer-id 14
+
+sudo ip r add ${DN_CIDR} dev gtp5gtest # traffic from RAN to DN, route to gtp5gtest
+
+echo "############### UPF Part ###############"
+${EXEC_NS2} ./gtp5g-link add gtp5gtest &
+sleep 0.1
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 123 --qfi 9 # 123 map the RAN settup
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 321 --qfi 9 # 321 map the RAN settup
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 11 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000 # 11 is qer-id
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 12 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000 # 12 is qer-id
+${EXEC_NS2} ./gtp5g-tunnel add far gtp5gtest 1 --action 2 # add far <ifname> <oid> [options...]
+${EXEC_NS2} ./gtp5g-tunnel add far gtp5gtest 2 --action 2 --hdr-creation 0 87 ${RAN_IP} 2152 # map to RAN f-teid
+${EXEC_NS2} ./gtp5g-tunnel add pdr gtp5gtest 1 --pcd 1 --hdr-rm 0 --ue-ipv4 ${UE_IP} --f-teid 78 ${UPF_IP} --far-id 1 --qer-id 11
+${EXEC_NS2} ./gtp5g-tunnel add pdr gtp5gtest 2 --pcd 2 --ue-ipv4 ${UE_IP} --far-id 2 --qer-id 12
+
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 124 --qfi 9
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 322 --qfi 9
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 13 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+${EXEC_NS2} ./gtp5g-tunnel add qer gtp5gtest 14 --qfi 1 --mbr-dl 1000000 --mbr-ul 1000000 --gbr-dl 500000 --gbr-ul 500000
+${EXEC_NS2} ./gtp5g-tunnel add far gtp5gtest 3 --action 2
+${EXEC_NS2} ./gtp5g-tunnel add far gtp5gtest 4 --action 2 --hdr-creation 0 88 ${RAN_IP} 2152
+${EXEC_NS2} ./gtp5g-tunnel add pdr gtp5gtest 3 --pcd 1 --hdr-rm 0 --ue-ipv4 ${UE2_IP} --f-teid 79 ${UPF_IP} --far-id 3 --qer-id 13
+${EXEC_NS2} ./gtp5g-tunnel add pdr gtp5gtest 4 --pcd 2 --ue-ipv4 ${UE2_IP} --far-id 4 --qer-id 14
+
+
+${EXEC_NS2} ip r add ${UE_CIDR} dev gtp5gtest # traffic from UPF to UE, route to gtp5gtest
+
+echo "############### Setup TC ###############"
+${EXEC_NS2} tc qdisc add dev gtp5gtest root handle 1: htb default 1
+${EXEC_NS2} tc class add dev gtp5gtest parent 1: classid 1:1 htb rate 1mbit
+${EXEC_NS2} tc qdisc add dev gtp5gtest parent 1:1 etf clockid CLOCK_TAI delta 100000000000 skip_sock_check
+
+echo "############### Test UP ###############"
+ping -c3 -I ${UE_IP} ${DN_IP} # ping -c3 -I 60.60.0.10 60.60.1.10
+ping -c3 -I ${UE2_IP} ${DN_IP} # ping -c3 -I 60.60.0.11 60.60.1.10
+
+echo "############## Stopping ##############"
+sleep 1
+sudo killall -15 gtp5g-link
+sleep 1
+
+if [ ${DUMP_NS} ]
+then
+   ${EXEC_NS2} kill -SIGINT ${TCPDUMP_PID}
+fi
+
+sudo ip link del gtp5gtest
+sudo ip link del veth0
+sudo ip netns del ${NS2}
+sudo ip addr del ${UE_IP}/32 dev lo
+sudo ip addr del ${UE2_IP}/32 dev lo
